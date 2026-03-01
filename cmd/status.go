0e28b6c772eb5595db132021491f204015af6f13
Teal Bauer <teal@starsong.eu>
2026-03-01 12:21:57 +0100

    feat: add status command for quick scooter overview

---
+++ cmd/status.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/rescoot/sunshine-cli/internal/output"
+	"github.com/spf13/cobra"
+)
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Show scooter status",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		id, err := resolveScooterID(nil)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+		scooter, err := apiClient.GetScooter(id)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+		output.PrintScooterDetail(scooter)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
